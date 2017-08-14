aaf6b6677020e031de05c0a0dd294820a327bb7e
Jonathan <lacombejonathan@yahoo.com>
2017-08-13 20:45:31 -0400

    Add missing integer tuples for last commit

---
+++ src/jonl/jutils/func/Tuple4i.java
@@ -0,0 +1,9 @@
+package jonl.jutils.func;
+
+public class Tuple4i extends Tuple4<Integer,Integer,Integer,Integer> {
+
+    public Tuple4i(int x, int y, int z, int w) {
+        super(x, y, z, w);
+    }
+    
+}
